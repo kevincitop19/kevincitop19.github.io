7f4a52a253ecf069e283739570fc34b2a08ede8e
LinoJaime <linojaime19@gmail.com>
2021-12-14 12:56:16 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const elementosCarousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(elementosCarousel, {
+      duration: 150,
+      numVisible: 3,
+      
+  }); 
+});
+
