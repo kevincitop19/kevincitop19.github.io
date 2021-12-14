77f4d902fad31d155b0f103856a0561dabe45c23
kevincitop19 <96127457+kevincitop19@users.noreply.github.com>
2021-12-14 10:09:14 -0500

    Delete Definitivo directory

--- Definitivo/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const elementosCarousel = document.querySelectorAll('.carousel');
-  M.Carousel.init(elementosCarousel, {
-      duration: 150,
-      numVisible: 3,
-      
-  }); 
-});
