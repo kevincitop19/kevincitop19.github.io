1232946507a160062ea8ae755d2da762f74fc4e6
LinoJaime <linojaime19@gmail.com>
2021-12-20 12:24:23 -0500

    version2.0

--- js/main.js
+++ js/main.js
@@ -7,3 +7,28 @@ document.addEventListener('DOMContentLoaded', () => {
   }); 
 });
 
+var modal=document.getElementById("myModal");
+  var modalImg=document.getElementById("img1");
+  var captionTxt=document.getElementById("caption");
+  var images = document.querySelectorAll(".images img");
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      modal.style.display="block";
+      modalImg.src = image.src;
+      captionTxt.innerHTML = image.alt;
+    });
+  });
+  var span=document.getElementsByClassName("close")[0];
+
+  span.onclick=function(){
+
+    modal.style.display="none";
+  }
+
+  /*img.onclick=function(){
+    modal.style.display="block";
+    modalImg.src=this.src;
+    captionText.innerHTML=this.alt;
+  }*/
+
+  
